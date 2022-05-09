d7b3289aee05059e920c0e71705c8740dd550fe7
Olga <igolnikova07@gmail.com>
2022-05-10 01:00:07 +0300

    code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class Program
+{
+    static void Main() {
+        
+        string[] inputArray = new string[] { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+        int size = inputArray.Length;
+        string[] outputArray = new string[size];
+        int minCount = 0;
+
+        for (int i = 0; i < size; i++)
+        {
+            if (inputArray[i].Length < 4)
+            {
+                
+                outputArray[minCount] = inputArray[i];
+                minCount++;
+            }
+        }
+
+        Console.Write("[");
+        Console.Write(string.Join(",", outputArray));
+        Console.WriteLine("]");
+    }
+}
